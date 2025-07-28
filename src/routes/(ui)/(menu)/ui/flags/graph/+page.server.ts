7994eddc4edb03a249b85739b0a2a09a3729c246
BCsabaEngine <csaba@balazs-magyar.hu>
2025-07-28 15:18:57 +0200

    fix: flag graph

--- src/routes/(ui)/(menu)/ui/flags/graph/+page.server.ts
+++ src/routes/(ui)/(menu)/ui/flags/graph/+page.server.ts
@@ -8,16 +8,13 @@ export const load: PageServerLoad = async ({ locals: { container } }) => {
 
 	const json = formatFlagApiResponseJson(flags);
 
-	const mermaidData: string[] = ['mindmap', '  flags'];
+	const mermaidData: string[] = ['mindmap', '  flags((Flags))'];
 	const objectToMermaid = (object: Record<string, unknown>, indent: number): void => {
-		for (const [key, value] of Object.entries(object)) {
+		for (const [key, value] of Object.entries(object))
 			if (typeof value === 'object' && value !== null) {
-				mermaidData.push(`${'  '.repeat(indent)}${key}`);
+				mermaidData.push('  '.repeat(indent) + key + '(' + key + ')');
 				objectToMermaid(value as Record<string, unknown>, indent + 1);
-			} else {
-				mermaidData.push(`${'  '.repeat(indent)}${key}`);
-			}
-		}
+			} else mermaidData.push('  '.repeat(indent) + key);
 	};
 	objectToMermaid(json as Record<string, unknown>, 2);
 
