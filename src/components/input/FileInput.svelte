4e3393fc6013fca4b58a5bcfc16ed86fc5e4bedd
BCsabaEngine <csaba@balazs-magyar.hu>
2025-08-03 16:51:58 +0200

    feat: file input component

---
+++ src/components/input/FileInput.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	interface Properties {
+		accept?: string;
+	}
+	const { accept = '' }: Properties = $props();
+
+	type OnSelect = (filename: string, data: string | undefined) => void;
+	export const selectFile = (onSelect: OnSelect) => {
+		fileInput.value = '';
+		_onSelect = onSelect;
+		fileInput.click();
+	};
+
+	let _onSelect: OnSelect | undefined;
+	let fileInput: HTMLInputElement;
+
+	const onchange = async (event: Event) => {
+		if (!_onSelect) return;
+		try {
+			const files = (event.target as HTMLInputElement).files;
+			if (files && files.length > 0) _onSelect(files[0].name, await files[0].text());
+		} catch {
+			/**/
+		}
+	};
+</script>
+
+<input bind:this={fileInput} id="textFile" style="display: none;" {accept} {onchange} type="file" />
