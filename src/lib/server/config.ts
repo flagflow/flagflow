485bb29dac098095eedb56d2f75fa040aafaacf3
BCsabaEngine <csaba@balazs-magyar.hu>
2025-08-10 14:54:07 +0200

    feat: config comments

--- src/lib/server/config.ts
+++ src/lib/server/config.ts
@@ -5,24 +5,34 @@ import envVar from 'env-var';
 
 export const config = {
 	logLevel: envVar.get('LOGLEVEL').default('info').asString(),
+
 	environment: envVar.get('ENVIRONMENT').default('').asString(),
+
+	// Remote migration settings
 	migration: {
 		sourceEnvironment: envVar.get('MIGRATION_SOURCE_ENVIRONMENT').default('').asString(),
 		sourceUrl: envVar.get('MIGRATION_SOURCE_URL').default('').asString()
 	},
+
 	etcd: {
 		server: envVar.get('ETCD_SERVER').default('localhost:2379').asString(),
 		username: envVar.get('ETCD_USERNAME').asString(),
 		password: envVar.get('ETCD_PASSWORD').asString(),
 		namespace: envVar.get('ETCD_NAMESPACE').default('default').asString()
 	},
+
+	// If you are using Keycloak for authentication
 	keycloak: {
 		host: envVar.get('KEYCLOAK_HOST').asString(),
 		realm: envVar.get('KEYCLOAK_REALM').default('master').asString(),
 		client: envVar.get('KEYCLOAK_CLIENT').default('flagflow-frontend').asString()
 	},
+
+	// Settings of in-built user session
 	session: {
+		// Enable user sessions
 		enabled: envVar.get('SESSION_USERS_ENABLED').default('true').asBool(),
+		// Default user (creation at startup) credentials
 		defaultUser: {
 			username: envVar.get('SESSION_DEFAULT_USERNAME').asString(),
 			password: envVar.get('SESSION_DEFAULT_PASSWORD').asString()
@@ -32,9 +42,11 @@ export const config = {
 			.default(30 * 60)
 			.asIntPositive()
 	},
+
 	metrics: {
 		enabled: envVar.get('METRICS_ENABLED').asBool() || false
 	},
+
 	dev: {
 		rpcSlowdownMs: envVar.get('DEV_RPC_SLOWDOWN_MS').asIntPositive()
 	}
