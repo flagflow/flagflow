08fc6f1227720f0db0f352113f0538be8ea90291
BCsabaEngine <csaba@balazs-magyar.hu>
2025-08-28 17:14:21 +0200

    feat: api login

---
+++ src/routes/api/login/+server.ts
@@ -0,0 +1,35 @@
+import { error } from '@sveltejs/kit';
+import z from 'zod';
+
+import { createJsonResponse } from '$lib/Response';
+import { zodFlattenError } from '$lib/zodEx';
+
+import type { RequestHandler } from './$types';
+
+const loginSchema = z.object({
+	username: z.string().trim(),
+	password: z.string()
+});
+
+export const POST: RequestHandler = async ({ request, locals }) => {
+	const loginData = loginSchema.safeParse(await request.json());
+	if (!loginData.success)
+		return error(400, `Invalid request body: ${zodFlattenError(loginData.error.issues)}`);
+
+	const userService = locals.container.resolve('userService');
+	const sessionService = locals.container.resolve('sessionService');
+	try {
+		const user = await userService.authUser(loginData.data.username, loginData.data.password);
+		const session = await sessionService.createSession({
+			userKey: user.username,
+			userName: user.username,
+			createdAt: new Date(),
+			permissions: user.permissions
+		});
+		return createJsonResponse({
+			sessionId: session
+		});
+	} catch (error_) {
+		return error(403, `${error_ instanceof Error ? error_.message : 'Login error'}`);
+	}
+};
