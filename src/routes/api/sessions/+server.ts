dd58f6d309a39a20bb4dd1db6ccc87053e05e4a9
BCsabaEngine <csaba@balazs-magyar.hu>
2025-08-30 15:15:29 +0200

    feat: restruct session list

--- src/routes/api/sessions/+server.ts
+++ src/routes/api/sessions/+server.ts
@@ -9,7 +9,19 @@ export const GET: RequestHandler = async ({ locals }) => {
 
 	const sessions = await locals.rpcCaller.session.getList();
 
+	const resultSessions: Record<string, unknown> = {};
+	for (const session of sessions)
+		resultSessions[session.key] = {
+			createdAt: session.createdAt,
+			expiredAt: new Date(session.expiredAt),
+			user: {
+				key: session.userKey,
+				name: session.userName,
+				permissions: session.permissions
+			}
+		};
+
 	return createJsonResponse({
-		sessions
+		sessions: resultSessions
 	});
 };
